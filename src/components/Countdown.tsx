3b9a9a0dd5c72b479f79a057d15aee76f0d6c4ef
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-02-24 17:17:22 +0100

    Countdown finished

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
+let countdownTimeout: NodeJS.Timeout // node.js.timeout -> tipo da variável
+
 export function Countdown() {
-   const [time, setTime] = useState(25 * 60) /* 25 minutos em segundos */
-   const [active, setActive] = useState(false)
+   const [time, setTime] = useState(0.05 * 60) /* 25 minutos em segundos */
+   const [isActive, setIsActive] = useState(false)
+   const [hasFinished, setHasFinished] = useState(false);
 
    const minutes = Math.floor(time / 60)
    const seconds = time % 60
@@ -12,21 +15,32 @@ export function Countdown() {
       [minuteLeft, minuteRight]-> o split separa a string num array, neste caso array de dois elementos. 
       [minuteLeft, minuteRight] é a desestruturação do array em duas variáveis
    */
-   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('') 
-   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('') 
+   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
    function startCountdown() {
-      setActive(true)
+      setIsActive(true)
+   }
+
+   function resetCountdown() {
+      // Evita a execução do timeout após um segundo. Ao setar isActive para false, o setTimeout já foi executado e irá diminuir ainda 1 segundo. 
+      // Aqui esse comportamento é cancelado
+      clearTimeout(countdownTimeout)
+      setIsActive(false);
+      setTime(0.05 * 60)
    }
 
    useEffect(() => {
-      if (active && time > 0) {
-         setTimeout(() => { /* setTimeout():  fazer com que algo acontece depois de um tempo (1000 milessegundos, 1 segundo) */
+      if (isActive && time > 0) {
+         countdownTimeout = setTimeout(() => { /* setTimeout():  fazer com que algo acontece depois de um tempo (1000 milessegundos, 1 segundo) */
             setTime(time - 1)
-         }, 1000) 
+         }, 1000)
+      } else if (isActive && time === 0) {
+         setHasFinished(true)
+         setIsActive(false)
       }
 
-   },[active, time])
+   }, [isActive, time])
 
    return (
       <div>
@@ -41,13 +55,38 @@ export function Countdown() {
                <span>{secondRight}</span>
             </div>
          </div>
-         <button 
-            type="button" 
-            className={styles.countdownButton}
-            onClick={startCountdown}
-         >
-            Iniciar um ciclo
-         </button>
+
+         { hasFinished ? (
+            <button
+               disabled
+               className={styles.countdownButton}
+            >
+               Ciclo encerrado
+            </button>
+         ) : (
+               <>
+                  { isActive ? (
+                     <button
+                        type="button"
+                        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                        onClick={resetCountdown}
+                     >
+                        Abandonar ciclo
+                     </button>
+                  ) : (
+                        <button
+                           type="button"
+                           className={styles.countdownButton}
+                           onClick={startCountdown}
+                        >
+                           Iniciar um ciclo
+                        </button>
+                     )
+                  }
+               </>
+            )
+         }
+
       </div>
    )
 }
