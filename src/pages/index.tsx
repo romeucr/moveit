4c33371e09d852cd5b2f5b8b78c64aa51c0bf3b3
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-02-25 10:33:34 +0100

    Challenge component layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/CountDown';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
@@ -19,7 +20,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
