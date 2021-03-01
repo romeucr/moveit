9acfb2d4116bc73a0bf0b95fc395692f86d5b289
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2021-03-01 12:09:07 +0100

    Salving user data on cookies and create LevelUpModal

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,9 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
 import challenges from '../challenges.json'
+import { LevelUpModal } from '../components/LevelUpModal'
+//quando a biblioteca importada apresenta esses ... é porque é feita em JS e não reconhece o Typescript. Os @types precisam ser instalados de uma terceira fonte: Definitely Type
+// é um repo da comunidade que contém tipagem TS de várias bibliotecas JS que não disponibilizam por padrão. É o caso da js-cookie
+import Cookies from 'js-cookie'
 
 interface challenge {
    type: 'body' | 'eye' //body é uma string, com só possui dois valores, aqui estão definidos ambos
@@ -9,7 +13,7 @@ interface challenge {
 
 interface ChallengesContextData {
    level: number
-   currentExperience: number 
+   currentExperience: number
    challengesCompleted: number
    activeChallenge: challenge //definido ali em cima
    experienceToNextLevel: number
@@ -17,43 +21,63 @@ interface ChallengesContextData {
    startNewChallenge: () => void
    resetChallenge: () => void
    completeChallenge: () => void
+   closeLevelUpModal: () => void
 }
 
 interface ChallengesProviderProps {
    //tipagem do children como ReactNode, porque lá no _app.tsx o <Component {...pageProps} /> é um componente React
-   children: ReactNode 
+   children: ReactNode
+   level: number
+   currentExperience: number
+   challengesCompleted: number
 }
 
 // quando criado o contexto, indica que é do tipo ChallengesContextData
 export const ChallengesContext = createContext({} as ChallengesContextData)
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-   const [level, setLevel] = useState(1)
-   const [currentExperience, setCurrentExperience] = useState(0)
-   const [challengesCompleted, setChallengesCompleted] = useState(0)
+//rest é um objeto que contém o resto das props, tirando children
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+   const [level, setLevel] = useState(rest.level ?? 1)
+   const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0)
+   const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
    const [activeChallenge, setActiveChallenge] = useState(null)
-   
+   const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false)
+
+
+   //calculando a experiencia para o proximo nivel
    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
    useEffect(() => {
       Notification.requestPermission() /* solicita permissão no browser para enviar notificações. API nativa do navegador */
-   },[])
+   }, [])
+
+   useEffect(() => {
+      //salvando as informações em cookies
+      Cookies.set('level', String(level))
+      Cookies.set('currentExperience', String(currentExperience))
+      Cookies.set('challengesCompleted', String(challengesCompleted))
+   }, [level, currentExperience, challengesCompleted])
+
+   function closeLevelUpModal() {
+      setIsLevelUpModalOpen(false)
+   }
 
    function levelUp() {
       setLevel(level + 1)
+      setIsLevelUpModalOpen(true)
    }
 
    function startNewChallenge() {
       const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
       const challenge = challenges[randomChallengeIndex]
-      
+
       setActiveChallenge(challenge)
 
       /* tocar um audio ao liberar novo desafio. Audio() nativo do navegador */
-      new Audio('/notification.mp3').play 
+      new Audio('/notification.mp3').play
 
       /* cria a notificação que será enviada ao usuário. nativo do navegador */
-      if ( Notification.permission === 'granted') {
+      if (Notification.permission === 'granted') {
          new Notification('Novo desafio liberado! 💪', {
             body: `Valendo ${challenge.amount}xp!`
          })
@@ -69,11 +93,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
          return //um return void
       }
 
-      const { amount } = activeChallenge   
+      const { amount } = activeChallenge
 
       let finalExperience = currentExperience + amount
 
-      if (finalExperience >= experienceToNextLevel ) {
+      if (finalExperience >= experienceToNextLevel) {
          finalExperience = finalExperience - experienceToNextLevel
          levelUp()
       }
@@ -84,19 +108,21 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
    }
 
    return (
-      <ChallengesContext.Provider 
+      <ChallengesContext.Provider
          value={{  /* tudo que o contexto disponibiliza */
-            level, 
-            currentExperience, 
-            challengesCompleted, 
+            level,
+            currentExperience,
+            challengesCompleted,
             activeChallenge,
             experienceToNextLevel,
             levelUp,
             startNewChallenge,
             resetChallenge,
-            completeChallenge
+            completeChallenge,
+            closeLevelUpModal
          }}>
          {children}
+         { isLevelUpModalOpen && <LevelUpModal />} {/* && if sem else */}
       </ChallengesContext.Provider>
 
    )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
@@ -6,26 +7,57 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
 import styles from '../styles/pages/Home.module.css';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomePageProps {
+  level: number
+  currentExperience: number
+  challengesCompleted: number
+}
+
+export default function Home(props: HomePageProps) {
   return (
-    <div className={styles.container}>
-      <Head> {/* //coloca na head do html tudo que estiver aqui. Aqui somente o que for da home. Em _document.tsx o que for global */}
-        <title>Início | move.it!</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head> {/* //coloca na head do html tudo que estiver aqui. Aqui somente o que for da home. Em _document.tsx o que for global */}
+          <title>Início | move.it!</title>
+        </Head>
+        <ExperienceBar />
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+// para recuperar os dados dos cookies quando der F5 na página. Só funciona em páginas, em componentes não
+// a função tem que ter esse nome e ser async. É uma das maneiras usadas quando utilizando Next.JS
+// são definidos os dados que podem ser repassados pela camada do Next.js ao frontend React
+// tudo executado na função é executado no lado do Next.js
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  //recupera todos os cookies da app
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level), //vem dos cookies como string, convertendo a number
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
